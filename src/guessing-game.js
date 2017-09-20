a1386c36134c94c0d15b76d86123ee9bb88ba94c
Pavel Ramasheuski <pavel.ramasheuski@gmail.com>
2017-09-20 22:40:53 +0300

    SolutionTry1

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,25 @@
 class GuessingGame {
-    constructor() {}
+    var max;
+	var min;
+    constructor() {max=0; min=0;}
 
-    setRange(min, max) {
-
+    setRange(min1, max1) {
+    min=min1;
+	max=max1;
     }
 
     guess() {
+	return Math.floor((max+min)/2);
 
     }
 
     lower() {
 
+	max=Math.floor((max+min)/2);
     }
 
     greater() {
-
+    min=Math.floor((max+min)/2);
     }
 }
 
